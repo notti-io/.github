578d6e5c5e23d2249c26eb8d5ec4d8cc19b029bd
kurrx <kurr.eax@gmail.com>
2024-03-03 16:29:41 +0500

    feat: added pointer related states to store

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,9 +1,22 @@
 import { getIsDebug } from '@/utils/state'
 import { create } from 'zustand'
+import Pointer from './Pointer'
 
 export interface Store {
   isDebug: boolean
   setIsDebug: (isDebug: boolean) => void
+
+  isPointerOut: boolean
+  setIsPointerOut: (isPointerOut: boolean) => void
+
+  isPointerDown: boolean
+  setIsPointerDown: (isPointerDown: boolean) => void
+
+  isHovering: boolean
+  setIsHovering: (isHovering: boolean) => void
+
+  pointer: Pointer
+  setPointerCoords: (x: number, y: number) => void
 }
 
 export const initialIsDebug = getIsDebug()
@@ -11,6 +24,22 @@ export const initialIsDebug = getIsDebug()
 const useStore = create<Store>(set => ({
   isDebug: initialIsDebug,
   setIsDebug: isDebug => set({ isDebug }),
+
+  isPointerOut: true,
+  setIsPointerOut: isPointerOut => set({ isPointerOut }),
+
+  isPointerDown: false,
+  setIsPointerDown: isPointerDown => set({ isPointerDown }),
+
+  isHovering: false,
+  setIsHovering: isHovering => set({ isHovering }),
+
+  pointer: new Pointer(),
+  setPointerCoords: (x, y) =>
+    set(state => {
+      state.pointer.set(x, y)
+      return state
+    }),
 }))
 
 export default useStore
