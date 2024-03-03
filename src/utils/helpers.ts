c014314c3efe4e0e0b3cf5ae686a7d7d132ff3fc
kurrx <kurr.eax@gmail.com>
2024-03-03 17:18:34 +0500

    feat: added classNames helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,3 +4,7 @@ export function mediaQuery(query: string) {
   if (!window) return false
   return window.matchMedia(query).matches
 }
+
+export function classNames(args: (string | undefined | null | boolean)[]) {
+  return args.filter(Boolean).join(' ')
+}
