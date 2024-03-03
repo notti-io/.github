851944d266013e8e0b676c86195f3a46b965272a
kurrx <kurr.eax@gmail.com>
2024-03-03 11:01:26 +0500

    feat: added env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FULL_NAME: string
+  readonly VITE_NICK_NAME: string
+  readonly VITE_POSITION_NAME: string
+  readonly VITE_TITLE: string
+  readonly VITE_DESCRIPTION: string
+  readonly VITE_COUNTRY: string
+
+  readonly VITE_SITE_URL: string
+  readonly VITE_EMAIL: string
+  readonly VITE_TWITTER_HANDLE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
