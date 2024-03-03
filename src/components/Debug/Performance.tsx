d726ed6e02aa3c0b74defc34d9c2e031d6e204b3
kurrx <kurr.eax@gmail.com>
2024-03-03 12:14:52 +0500

    feat: added Performance component

---
+++ src/components/Debug/Performance.tsx
@@ -0,0 +1,12 @@
+import { AdaptiveDpr } from '@react-three/drei'
+import { Fragment } from 'react'
+
+function Performance() {
+  return (
+    <Fragment>
+      <AdaptiveDpr />
+    </Fragment>
+  )
+}
+
+export default Performance
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,10 @@
 import { Canvas } from '@react-three/fiber'
+import Performance from './Debug/Performance'
 
 function Experience() {
   return (
     <Canvas flat id='experience' camera={{ fov: 35, near: 0.1, far: 500 }}>
+      <Performance />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshBasicMaterial color='red' />
