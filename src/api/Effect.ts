48b651706547d881f8d25e7a31857f9c78c40186
kurrx <kurr.eax@gmail.com>
2024-03-04 19:50:26 +0500

    feat: added Effect class to create custom PP effects

---
+++ src/api/Effect.ts
@@ -0,0 +1,48 @@
+import { Effect as EffectImpl } from 'postprocessing'
+import type { BlendFunction, EffectAttribute, WebGLExtension } from 'postprocessing'
+import type { Uniform } from 'three'
+import Shader from './Shader'
+
+export interface EffectCoreOptions {
+  vertexShader?: string
+  attributes?: EffectAttribute
+  blendFunction?: BlendFunction
+  defines?: Map<string, string>
+  uniforms?: Map<string, Uniform>
+  extensions?: Set<WebGLExtension>
+}
+
+export interface EffectCoreArgs extends EffectCoreOptions {
+  name: string
+  fragmentShader: string
+}
+
+class Effect extends EffectImpl {
+  constructor(args: EffectCoreArgs) {
+    super(...Effect.extendArgs(args))
+  }
+
+  get time() {
+    return this.uniforms.get('time')!.value
+  }
+  set time(v: number) {
+    this.uniforms.get('time')!.value = v
+  }
+
+  get DPR() {
+    return this.uniforms.get('DPR')!.value
+  }
+  set DPR(v: number) {
+    this.uniforms.get('DPR')!.value = v
+  }
+
+  private static extendArgs(args: EffectCoreArgs) {
+    args.fragmentShader = Shader.extend(args.fragmentShader)!
+    args.vertexShader = Shader.extend(args.vertexShader)
+    args.uniforms = Shader.extendEffectUniforms(args.uniforms)
+
+    return [args.name, args.fragmentShader, args] as const
+  }
+}
+
+export default Effect
