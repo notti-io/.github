e1f7cf2859c9a8d26bf7682474318f17a0b0ee8e
kurrx <kurr.eax@gmail.com>
2024-03-04 19:55:32 +0500

    feat: added blending shader utils

---
+++ src/utils/shader/blending.glsl
@@ -0,0 +1,36 @@
+float blendOverlay(float base, float blend) {
+  return base < 0.5 ? (2.0 * base * blend) : (1.0 - 2.0 * (1.0 - base) * (1.0 - blend));
+}
+vec3 blendOverlay(vec3 base, vec3 blend) {
+  return vec3(blendOverlay(base.r, blend.r), blendOverlay(base.g, blend.g), blendOverlay(base.b, blend.b));
+}
+vec3 blendOverlay(vec3 base, vec3 blend, float opacity) {
+  return (blendOverlay(base, blend) * opacity + base * (1.0 - opacity));
+}
+
+vec3 blendMultiply(vec3 base, vec3 blend) {
+  return base * blend;
+}
+vec3 blendMultiply(vec3 base, vec3 blend, float opacity) {
+  return (blendMultiply(base, blend) * opacity + base * (1.0 - opacity));
+}
+
+float blendSoftLight(float base, float blend) {
+  return (blend < 0.5) ? (2.0 * base * blend + base * base * (1.0 - 2.0 * blend)) : (sqrt(base) * (2.0 * blend - 1.0) + 2.0 * base * (1.0 - blend));
+}
+vec3 blendSoftLight(vec3 base, vec3 blend) {
+  return vec3(blendSoftLight(base.r, blend.r), blendSoftLight(base.g, blend.g), blendSoftLight(base.b, blend.b));
+}
+vec3 blendSoftLight(vec3 base, vec3 blend, float opacity) {
+  return (blendSoftLight(base, blend) * opacity + base * (1.0 - opacity));
+}
+
+float blendSubtract(float base, float blend) {
+  return max(base + blend - 1.0, 0.0);
+}
+vec3 blendSubtract(vec3 base, vec3 blend) {
+  return max(base + blend - vec3(1.0), vec3(0.0));
+}
+vec3 blendSubtract(vec3 base, vec3 blend, float opacity) {
+  return (blendSubtract(base, blend) * opacity + base * (1.0 - opacity));
+}
