17cd3849e65154e2c6d4e349570492105798fa1c
kurrx <kurr.eax@gmail.com>
2024-03-04 19:56:35 +0500

    feat: added new utility functions to compiler

--- src/utils/shader/index.ts
+++ src/utils/shader/index.ts
@@ -1,7 +1,10 @@
+import colorConvert from './colorConvert.glsl?raw'
+import blending from './blending.glsl?raw'
 import curlNoise from './curlNoise.glsl?raw'
+import easing from './easing.glsl?raw'
 import range from './range.glsl?raw'
 import rotateUv from './rotateUv.glsl?raw'
 
-const shaderUtils: Record<string, string> = { curlNoise, range, rotateUv }
+const shaderUtils: Record<string, string> = { colorConvert, blending, curlNoise, easing, range, rotateUv }
 
 export default shaderUtils
