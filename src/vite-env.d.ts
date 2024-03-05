de2c635fe0dd9e687cb4132045a7eca15c3082ed
kurrx <kurr.eax@gmail.com>
2024-03-05 10:52:53 +0500

    feat: added city as env var

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_TITLE: string
   readonly VITE_DESCRIPTION: string
   readonly VITE_COUNTRY: string
+  readonly VITE_CITY: string
 
   readonly VITE_SITE_URL: string
   readonly VITE_EMAIL: string
