5dd6e08c883ef46113d0ce8a425353b30db8eb8f
kurrx <kurr.eax@gmail.com>
2024-03-06 16:25:48 +0500

    feat: added fixed props to navbar button

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -5,16 +5,17 @@ import { useCursor } from '@/hooks/useCursor'
 export interface NavbarButtonProps extends PropsWithChildren {
   id: string
   side: 'left' | 'right'
+  fixed?: boolean
   onClick: () => void
 }
 
-function NavbarButton({ id, side, children, onClick }: NavbarButtonProps) {
+function NavbarButton({ id, side, fixed = true, children, onClick }: NavbarButtonProps) {
   const ref = useRef<HTMLButtonElement>(null)
 
   useCursor(id, ref)
 
   return (
-    <button ref={ref} className={`navbar-button navbar-button-${side}`} onClick={onClick}>
+    <button ref={ref} className={`navbar-button navbar-button-${fixed ? 'fixed' : 'static'} navbar-button-${side}`} onClick={onClick}>
       {children}
       <span className='navbar-button-aura' />
     </button>
