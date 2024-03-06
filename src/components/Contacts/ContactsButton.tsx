1fa4c50aff95a08c24ac51219f4ab80b474f8c27
kurrx <kurr.eax@gmail.com>
2024-03-06 23:00:49 +0500

    refactor: optimized props

--- src/components/Contacts/ContactsButton.tsx
+++ src/components/Contacts/ContactsButton.tsx
@@ -5,6 +5,15 @@ import { useCursor } from '@/hooks/useCursor'
 
 const transition = { ease: [0.005, 0.985, 0.22, 1], duration: 0.6 }
 const variants = (y: '100%' | '-100%') => ({ hidden: { y, opacity: 0 }, visible: { y: 0, opacity: 1 } })
+const textProps = (main: boolean) => (open: boolean) => ({
+  className: `contacts-button-text${main ? '' : '-close'}`,
+  variants: variants(main ? '-100%' : '100%'),
+  initial: main ? 'visible' : 'hidden',
+  animate: main ? (open ? 'hidden' : 'visible') : open ? 'visible' : 'hidden',
+  transition,
+})
+const mainProps = textProps(true)
+const closeProps = textProps(false)
 
 function ContactsButton() {
   const ref = useRef<HTMLButtonElement>(null)
@@ -19,24 +28,8 @@ function ContactsButton() {
 
   return (
     <button ref={ref} className='contacts-button navbar-text' onClick={toggle}>
-      <motion.span
-        className='contacts-button-text'
-        variants={variants('-100%')}
-        initial='visible'
-        animate={open ? 'hidden' : 'visible'}
-        transition={transition}
-      >
-        Contact
-      </motion.span>
-      <motion.span
-        className='contacts-button-text-close'
-        variants={variants('100%')}
-        initial='hidden'
-        animate={!open ? 'hidden' : 'visible'}
-        transition={transition}
-      >
-        Close-x
-      </motion.span>
+      <motion.span {...mainProps(open)}>Links</motion.span>
+      <motion.span {...closeProps(open)}>Close</motion.span>
       <span className='contacts-button-aura' />
     </button>
   )
