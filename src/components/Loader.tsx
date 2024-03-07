a8e80440a82f9f1c1c1ee5e460d017c3a3b49764
kurrx <kurr.eax@gmail.com>
2024-03-07 19:43:50 +0500

    feat: added Loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,32 +1,52 @@
-import { PropsWithChildren, useEffect, useRef } from 'react'
+import { animate, motion, useMotionValue } from 'framer-motion'
+import { Fragment, PropsWithChildren, useEffect, useRef } from 'react'
 import useStore from '@/api/store'
 import Shader from '@/api/Shader'
 
 function Loader({ children }: PropsWithChildren) {
+  const counterRef = useRef<HTMLElement>(null)
   const isLoading = useStore(state => state.isLoading)
-  const timeout = useRef<NodeJS.Timeout | null>(null)
   const setIsLoading = useStore(state => state.setIsLoading)
+  const y = useMotionValue('0%')
+  const display = useMotionValue('block')
 
   useEffect(() => {
-    if (timeout.current) {
-      clearTimeout(timeout.current)
-      timeout.current = null
-    }
-    timeout.current = setTimeout(() => {
-      Shader.translateLoader(() => setIsLoading(false))
-    }, 1000)
+    const controls = animate(0, 100, {
+      ease: 'easeInOut',
+      duration: 1,
+      delay: 0.3,
+      onUpdate(value) {
+        if (counterRef.current) {
+          counterRef.current.textContent = `${Math.round(value)}%`
+        }
+      },
+      onComplete() {
+        animate(y, '100%', {
+          ease: 'easeInOut',
+          duration: 0.6,
+          delay: 0.3,
+          onComplete() {
+            Shader.translateLoader(() => setIsLoading(false))
+            display.set('none')
+          },
+        })
+      },
+    })
 
-    return () => {
-      if (timeout.current) {
-        clearTimeout(timeout.current)
-        timeout.current = null
-      }
-    }
+    return () => controls.stop()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setIsLoading])
 
-  if (isLoading) return null
-
-  return children
+  return (
+    <Fragment>
+      <motion.div className='loader' style={{ display }}>
+        <motion.div style={{ y }}>
+          <span ref={counterRef}>0%</span>
+        </motion.div>
+      </motion.div>
+      {!isLoading && children}
+    </Fragment>
+  )
 }
 
 export default Loader
