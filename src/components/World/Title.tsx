61c93fb1d7222a90a2fc702413f0259096daff9f
kurrx <kurr.eax@gmail.com>
2024-03-07 17:37:55 +0500

    feat: added Text fill opacity intro animation

--- src/components/World/Title.tsx
+++ src/components/World/Title.tsx
@@ -1,8 +1,10 @@
 import { Text } from '@react-three/drei'
-import { GroupProps } from '@react-three/fiber'
-import { useMemo } from 'react'
+import { GroupProps, useFrame } from '@react-three/fiber'
+import { useMemo, useRef } from 'react'
+import Shader from '@/api/Shader'
 
 const fontUrl = '/fonts/StaffX/Italic.woff'
+const color = '#ffffff'
 
 export interface TitleProps extends GroupProps {
   screenWidth: number
@@ -10,15 +12,27 @@ export interface TitleProps extends GroupProps {
 
 function Title(props: TitleProps) {
   const { screenWidth, name = 'Title', ...restProps } = props
+  const firstLineRef = useRef<{ fillOpacity?: number }>(null)
+  const secondLineRef = useRef<{ fillOpacity?: number }>(null)
   const fontSize = useMemo(() => screenWidth / 10, [screenWidth])
   const lineSpacing = useMemo(() => fontSize / 2, [fontSize])
+  const prevOpacity = useRef<number>(-1)
+
+  useFrame(() => {
+    if (!firstLineRef.current || !secondLineRef.current) return
+    const opacity = Shader.getLoader()
+    if (prevOpacity.current !== opacity) {
+      firstLineRef.current.fillOpacity = opacity
+      secondLineRef.current.fillOpacity = opacity
+    }
+  })
 
   return (
     <group name={name} {...restProps}>
-      <Text position={[0, lineSpacing, 0]} font={fontUrl} fontSize={fontSize}>
+      <Text ref={firstLineRef} position={[0, lineSpacing, 0]} font={fontUrl} fontSize={fontSize} color={color}>
         KURBANALI
       </Text>
-      <Text position={[0, -lineSpacing, 0]} font={fontUrl} fontSize={fontSize}>
+      <Text ref={secondLineRef} position={[0, -lineSpacing, 0]} font={fontUrl} fontSize={fontSize} color={color}>
         RUSLAN
       </Text>
     </group>
